456c37d0e254bc758f37ecd14e258d760ab2f7f9
Kay-is-Shall <61397531+Kay-is-Shall@users.noreply.github.com>
2020-03-12 09:35:07 +0700

    json

--- maujson.py
+++ maujson.py
@@ -16,8 +16,8 @@ for headline in obj.find_all('div',class_='teaser_conten1'):
     date = datetime.now()
     dt_string = date.strftime("%A, %d %B %Y %H:%M:%S")
     news['Waktu Ambil'] = dt_string
-    temp = (str(news))
-    list.append(temp)
+    
+    list.append(dict(news))
     with open('headlinekayis.json', 'w') as file:
         json.dump(list, file, indent=4)
 
